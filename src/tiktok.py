e72d094facbdb398e453a59e4e3614ddbba61687
Mohammed <88824957+majhcc@users.noreply.github.com>
2022-03-27 18:31:19 +0400

    add new method

---
+++ src/tiktok.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    # add useragent
+    browser = p.chromium.launch(headless=False, slow_mo=500)
+    browser = browser.new_context(
+        user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
+    page = browser.new_page()
+    page.goto("http://tiktok.com/@majhc")
+    latest_video = page.locator('/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/a').inner_text
+    print(latest_video)
+    browser.close()
