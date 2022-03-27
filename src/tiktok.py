50e436c7df140197d7894b032fc1d6d143a7107d
Mohammed <88824957+majhcc@users.noreply.github.com>
2022-03-27 18:33:28 +0400

    update

--- src/tiktok.py
+++ src/tiktok.py
@@ -7,6 +7,6 @@ with sync_playwright() as p:
         user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
     page = browser.new_page()
     page.goto("http://tiktok.com/@majhc")
-    latest_video = page.locator('/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/a').inner_text
+    latest_video = page.query_selector('xpath=/html/body/div[2]/div[2]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/a').inner_text
     print(latest_video)
     browser.close()
